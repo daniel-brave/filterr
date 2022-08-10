e8ebe7f704551917919e14d6523636e7fb19b15b
Daniel Scott <daniel.scott.dr@gmail.com>
2022-08-10 16:53:02 +0530

    documentation

--- routes/content-moderation.js
+++ routes/content-moderation.js
@@ -1,44 +1,49 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var jsonStore = require('../json-store');
-const https = require('https');
-const { CID } = require('multiformats/cid')
-var fs = require('fs');
-var path = require('path');
+var jsonStore = require("../json-store");
+const https = require("https");
+const { CID } = require("multiformats/cid");
+var fs = require("fs");
+var path = require("path");
 
-router.get('/add', function (req, res, next) {
-
+router.get("/add", function (req, res, next) {
   if (!req.query.cid) {
-    res.status(404).send('Send a query string as cid');
+    res.status(404).send("Send a query string as cid");
     return;
   }
   let cid = req.query.cid;
   let verifiedCid = cid && CID.asCID(cid);
   if (verifiedCid) {
-    res.status(404).send('Invalid cid');
+    res.status(404).send("Invalid cid");
     return;
   }
 
-  analyzeImage(cid).then(analysis => {
-    if (analysis && analysis.adult && analysis.adult.isAdultContent === false && analysis.adult.isRacyContent === false) {
+  // Analyse an image
+  analyzeImage(cid).then((analysis) => {
+    if (
+      analysis &&
+      analysis.adult &&
+      analysis.adult.isAdultContent === false &&
+      analysis.adult.isRacyContent === false
+    ) {
       // add to bloom filter
       this.bloomFilter.add(cid);
 
       this.bloomFilterCounter++;
       let newbf = {
         bloomFilter: [].slice.call(this.bloomFilter.buckets),
-        bloomFilterCounter: this.bloomFilterCounter
-      }
+        bloomFilterCounter: this.bloomFilterCounter,
+      };
       jsonStore.update(newbf);
-      res.send(cid + ' - CID is valid.\nCID has been added to bloom filter');
+      res.send(cid + " - CID is valid.\nCID has been added to bloom filter");
     } else {
-      res.send('Rejected. Not Added cid: ' + cid);
+      res.send("Rejected. Not Added cid: " + cid);
     }
-  })
-
+  });
 });
 
-router.get('/test', function (req, res, next) {
+// Test the API routes
+router.get("/test", function (req, res, next) {
   if (req.query.cid) {
     let cid = req.query.cid;
     // console.log('cid for testing', cid);
@@ -46,47 +51,56 @@ router.get('/test', function (req, res, next) {
       let isPresent = this.bloomFilter.test(cid);
       res.send(isPresent);
     } else {
-      res.status(404).send('Submit a valid cid');
+      res.status(404).send("Submit a valid cid");
     }
   } else {
-    res.status(404).send('Send a query string as cid');
+    res.status(404).send("Send a query string as cid");
   }
 });
 
-router.get('/script', function(req, res, next) {
-  var templatePath = path.join(__dirname, '..', 'bloomfilter_script_template_min.js');
+// Script the code
+router.get("/script", function (req, res, next) {
+  var templatePath = path.join(
+    __dirname,
+    "..",
+    "bloomfilter_script_template_min.js"
+  );
   let script = fs.readFileSync(templatePath).toString();
-  script = script.replace('[LATEST_BLOOMFILTER_ARRAY]', JSON.stringify([].slice.call(this.bloomFilter.buckets)));
-  script = script.replace('[LATEST_BLOOMFILTER_COUNT]', this.bloomFilterCounter);
+  script = script.replace(
+    "[LATEST_BLOOMFILTER_ARRAY]",
+    JSON.stringify([].slice.call(this.bloomFilter.buckets))
+  );
+  script = script.replace(
+    "[LATEST_BLOOMFILTER_COUNT]",
+    this.bloomFilterCounter
+  );
   res.send(script);
 });
 
 function analyzeImage(cidUrl) {
   return new Promise((resolve, reject) => {
-
     // const data = JSON.stringify({ url: "https://ipfs.io/ipfs/QmSygLmdfeStPU7TTCTbazvrUCggtGh5aNEK6cLarWZVsX" });
-    const data = JSON.stringify({ url: 'https://ipfs.io/ipfs/' + cidUrl });
+    const data = JSON.stringify({ url: "https://ipfs.io/ipfs/" + cidUrl });
 
     const options = {
-      protocol: 'https:',
-      hostname: 'southeastasia.api.cognitive.microsoft.com',
+      protocol: "https:",
+      hostname: "southeastasia.api.cognitive.microsoft.com",
       port: 443,
-      path: '/vision/v3.2/analyze?visualFeatures=Adult&language=en&model-version=latest',
-      method: 'POST',
+      path: "/vision/v3.2/analyze?visualFeatures=Adult&language=en&model-version=latest",
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': data.length,
-        'Ocp-Apim-Subscription-Key': process.env.AZURE_CV_KEY
-      }
+        "Content-Type": "application/json",
+        "Content-Length": data.length,
+        "Ocp-Apim-Subscription-Key": process.env.AZURE_CV_KEY,
+      },
     };
 
-    const req = https.request(options, res => {
-
-      res.on('data', d => {
+    const req = https.request(options, (res) => {
+      res.on("data", (d) => {
         let metadata = JSON.parse(d);
         // console.log(metadata);
         resolve(metadata);
-      })
+      });
     });
 
     req.on("error", (error) => {
@@ -94,20 +108,17 @@ function analyzeImage(cidUrl) {
       reject();
     });
 
-    req.write(data)
+    req.write(data);
     req.end();
-  })
-
+  });
 }
 
-router.get('/cid', function(req, res, next) {
- if(this.latestCID) {
-  res.send(this.latestCID);
- } else {
-  res.send('Not yet there');
- }
-  
+router.get("/cid", function (req, res, next) {
+  if (this.latestCID) {
+    res.send(this.latestCID);
+  } else {
+    res.send("Not yet there");
+  }
 });
-
 
 module.exports = router;
